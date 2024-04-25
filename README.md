3a7689143150a6e7713914bd325477eb4f0abb7e
yeetssite <jacobhache09@gmail.com>
2024-04-24 22:24:35 -0600

    My brain hurts 💀

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# scripts
+# scripts  
 My bash and other assorted scripts
+
+# use  
+This repo is actually a github pages site (or at least it's gonna be) located at <`https://yeetssite.github.io/scripts/`>, but it has no content as it's specifically made to host files only.
+
+It's meant to be easy to use with bash, but can contain other script types, too.
+
+The idea is that you simply use something like `cURL -O https://yeetssite.github.io/scripts/[SCRIPT]` and you can replace `[SCRIPT]` with a simple script name, no version numbers, no funky special characters, just a simple name like `yeetsmemes.sh`.
+
+it's still a WIP and I'll work on it soon, but its 12:00a.m. at the time of writing this, and I like sleep.
---
+++ yeetsmemesdownloader.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# **COMING SOON, STILL A W.I.P!
+#
+# This script copyright (c)  2024 Jacob "yeetsup" Hache
+# See <LICENSE> stored in root folder, or at:
+# <http://fsf.org/licenses/gpl-2.0>
+#
+# Want to learn bash scripting? Too bad, kid, look it
+# up at: 
+# <http://google.com/search?q=How+to+bash+script?>
+# 
+# =================
+# About this script
+# =================
+#
+# What does it do?
+# 
+# It takes my memes, and downloads them to whatever 
+# folder you choose. 
+# [CONCEPT] You can pick which ones you want and 
+# open them in your web browser or program of choice,
+# be that an image editor or viewer, idc its ur choice,
+# you can type in any program if its installed (and if
+# it can open basic images, like .png's or .jpeg's, 
+# although it'd be nice if it could open .webp's, too)
+# and it will open it in that program.
+
+
